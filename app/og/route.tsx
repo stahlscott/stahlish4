c15254f48c09131ac9eb7ad6d0eb3c9dac7e664d
Scott Stahl <stahl@hey.com>
2025-05-09 15:25:29 -0400

    fixing build

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,15 +1,12 @@
 import { ImageResponse } from 'next/og'
 
-export function GET(request: Request) {
-  let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Scott Stahl'
-
+export function GET() {
   return new ImageResponse(
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
         <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
           <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
-            {title}
+            Scott Stahl
           </h2>
         </div>
       </div>
